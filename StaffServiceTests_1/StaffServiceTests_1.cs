6347860bd40aeaf6960d8f303bce58902265de28
aao <aao@skbkontur.ru>
2023-03-02 17:41:20 +0500

    create new repository

---
+++ StaffServiceTests_1/StaffServiceTests_1.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+
+namespace StaffServiceTests_1;
+
+public class StaffServiceTests
+{
+    [Test]
+    public void Authorization()
+    {
+        var options = new ChromeOptions();
+        options.AddArguments("--no-sandbox", "--start-maximized", "--disable-extensions");
+        var driver = new ChromeDriver(options);
+
+        driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru");
+
+        Thread.Sleep(3000);
+        driver.Quit();
+    }
+}
